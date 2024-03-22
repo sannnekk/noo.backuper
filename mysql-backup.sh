38e213b3074b81e23a039810901dd24f42f986cf
Mona Lisa <nukleoid@outlook.com>
2024-03-22 12:42:38 +0100

    [FIX] Privileges error

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysqldump -u$DB_USER root -p$DB_PASSWORD --lock-tables=false -h $DB_HOST $DB_NAME > /var/backup/backup-$(date +"%Y-%m-%d_%H.%M.%S").sql
+mysqldump -u$DB_USER root -p$DB_PASSWORD --lock-tables=false --no-tablespaces -h $DB_HOST $DB_NAME > /var/backup/backup-$(date +"%Y-%m-%d_%H.%M.%S").sql
