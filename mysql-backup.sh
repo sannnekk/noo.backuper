d4289729c900680490483aadf7360789f234dd98
Mona Lisa <nukleoid@outlook.com>
2024-03-22 12:51:44 +0100

    [FIX]

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-mysqldump -u$DB_USER root -p$DB_PASSWORD --lock-tables=false --no-tablespaces -h $DB_HOST $DB_NAME > /var/backup/backup-$(date +"%Y-%m-%d_%H.%M.%S").sql
+echo "Dumping MySQL database: $DB_NAME, user: $DB_USER, host: $DB_HOST"
+
+mysqldump -u$DB_USER -p$DB_PASSWORD --lock-tables=false --no-tablespaces -h $DB_HOST $DB_NAME > /var/backup/backup-$(date +"%Y-%m-%d_%H.%M.%S").sql
