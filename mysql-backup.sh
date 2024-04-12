95d40dfe550819957ea6e9c0bbade63364585452
Mona Lisa <nukleoid@outlook.com>
2024-04-12 11:02:20 +0200

    [IMPR] Added removing of backups older than 12 hours

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -3,3 +3,10 @@
 echo "Dumping MySQL database: $DB_NAME, user: $DB_USER, host: $DB_HOST"
 
 mysqldump -u$DB_USER -p$DB_PASSWORD --lock-tables=false --no-tablespaces -h $DB_HOST $DB_NAME > /var/backup/backup-$(date +"%Y-%m-%d_%H.%M.%S").sql
+
+echo "MySQL database dumped successfully"
+echo "Removing backups older than 12 hours"
+
+find /var/backup -name "backup-*.sql" -type f -mmin +720 -exec rm {} \;
+
+echo "Removed backups older than 12 hours"
