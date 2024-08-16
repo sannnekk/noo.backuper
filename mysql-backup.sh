406a76d4eb6a13a1bf046f97aef905460a509204
sannnekk <nukleoid@outlook.com>
2024-08-16 10:01:45 +0200

    [IMPR] Changed to last 6 hours. TODO: send to bucket

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -5,8 +5,8 @@ echo "Dumping MySQL database: $DB_NAME, user: $DB_USER, host: $DB_HOST"
 mysqldump -u$DB_USER -p$DB_PASSWORD --lock-tables=false --no-tablespaces -h $DB_HOST $DB_NAME > /var/backup/backup-$(date +"%Y-%m-%d_%H.%M.%S").sql
 
 echo "MySQL database dumped successfully"
-echo "Removing backups older than 12 hours"
+echo "Removing backups older than 6 hours"
 
-find /var/backup -name "backup-*.sql" -type f -mmin +720 -exec rm {} \;
+find /var/backup -name "backup-*.sql" -mmin +360 -exec rm {} \;
 
-echo "Removed backups older than 12 hours"
+echo "Removed backups older than 6 hours"
