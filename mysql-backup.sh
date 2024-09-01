80403b35cb7189a787dbdbea954cee4d1c3e3bfb
sannnekk <nukleoid@outlook.com>
2024-09-01 02:10:38 +0200

    Added gzip

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -2,11 +2,11 @@
 
 echo "Dumping MySQL database: $DB_NAME, user: $DB_USER, host: $DB_HOST"
 
-mysqldump -u$DB_USER -p$DB_PASSWORD --lock-tables=false --no-tablespaces --single-transaction --max_allowed_packet=5G --quick -h $DB_HOST $DB_NAME > /var/backup/backup-$(date +"%Y-%m-%d_%H.%M").sql
+mysqldump -u$DB_USER -p$DB_PASSWORD --lock-tables=false --no-tablespaces --max_allowed_packet=5G --quick -h $DB_HOST $DB_NAME | gzip > /var/backup/backup-$(date +%Y-%m-%d-%H-%M-%S).sql.gz
 
 echo "MySQL database dumped successfully"
-echo "Removing backups older than 6 hours"
+echo "Removing backups older than 12 hours"
 
-find /var/backup -name "backup-*.sql" -type f -mmin +360 -exec rm {} \;
+find /var/backup -name "backup-*.sql" -type f -mmin +720 -exec rm {} \;
 
 echo "Removed backups older than 6 hours"
