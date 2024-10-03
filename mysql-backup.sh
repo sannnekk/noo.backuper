2de1874fed18bc4ad771c06981c30c66ff7319c4
sannnekk <nukleoid@outlook.com>
2024-10-03 17:19:41 +0200

    [IMPR] Added master data and single transacion options

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -2,7 +2,7 @@
 
 echo "Dumping MySQL database: $DB_NAME, user: $DB_USER, host: $DB_HOST"
 
-mysqldump -u$DB_USER -p$DB_PASSWORD --lock-tables=false --no-tablespaces --max_allowed_packet=5G --quick -h $DB_HOST $DB_NAME | gzip > /var/backup/backup-$(date +%Y-%m-%d-%H-%M-%S).sql.gz
+mysqldump -u$DB_USER -p$DB_PASSWORD --lock-tables=false --no-tablespaces --max_allowed_packet=5G --quick --single-transaction --master-data=2 -h $DB_HOST $DB_NAME | gzip > /var/backup/backup-$(date +%Y-%m-%d-%H-%M-%S).sql.gz
 
 echo "MySQL database dumped successfully"
 echo "Removing backups older than 12 hours"
