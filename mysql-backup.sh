cd0c66e20843924411bde67b948bb3e389f1e6d0
sannnekk <nukleoid@outlook.com>
2024-10-06 00:49:22 +0200

    [IMPR] Added more info

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo "Dumping MySQL database: $DB_NAME, user: $DB_USER, host: $DB_HOST"
+echo "Options: --single-transaction --set-gtid-purged=ON --max_allowed_packet=5G"
+echo "Backup file: /var/backup/backup-$(date +%Y-%m-%d-%H-%M-%S).sql.gz"
+echo "Mysqldump version: $(mysqldump --version)" 
 
-mysqldump -u$DB_USER -p$DB_PASSWORD --single-transaction --flush-logs --set-gtid-purged=ON --max_allowed_packet=5G -h $DB_HOST $DB_NAME | gzip > /var/backup/backup-$(date +%Y-%m-%d-%H-%M-%S).sql.gz
+mysqldump -u$DB_USER -p$DB_PASSWORD --single-transaction --set-gtid-purged=ON --max_allowed_packet=5G -h $DB_HOST $DB_NAME | gzip > /var/backup/backup-$(date +%Y-%m-%d-%H-%M-%S).sql.gz
 
-echo "MySQL database dumped successfully"
-echo "Removing backups older than 12 hours"
-
-find /var/backup -name "backup-*.sql" -type f -mmin +720 -exec rm {} \;
-
-echo "Removed backups older than 6 hours"
+# if [ $? -eq 0 ]; then
+#   echo "Backup successful"
+# else
+#   echo "Backup failed"
+# fi
